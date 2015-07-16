a0776c563106d8615c20642e3961335f3350dfdf
covalwill <klimtgit@gmail.com>
2015-07-16 08:04:30 -0700

    minor changes

--- js/app.js
+++ js/app.js
@@ -1,49 +1,36 @@
-//cache the variable
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
 
-var $password = $("password")
-
-var $confirmPassword = $("confirm_password")
-
-
-
-
-
-$("form span").hide(); //Hides the span tag
-
-
-
-
-
-
+//Hides the span tag
+$("form span").hide(); 
 
 // function that will validate and check for a character length of 8. If there is a length of more than 8 it will hide the span tag. 
 function passwordEvent() {
 
-	if ($password.val().length > 8) {
-		
-		$password.next().hide();
-	} else {
+    if($password.val().length > 8) {
 
-		$password.next().show();
-	}
+      $password.next().hide();
+    } else {
+      
+      $password.next().show();
+    }
 }
 
-//
-function confirmPasswordEvent() {
 
-	if ($password.val() === $confirmPassword.val()) {
-		
-		$confirmPassword.next().hide();
-	} else {
+function confirmPasswordEvent(){
 
-		$confirmPassword.next().show();
-	}
+  if($password.val() === $confirmPassword.val()) {
 
+    $confirmPassword.next().hide();
+  } else{
+
+    $confirmPassword.next().show();
+  }
 }
  
 
 //Password
-$password.focus(passwordEvent).keyup(passwordEvent);
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
 
 
 //Confirm Password
