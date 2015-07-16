06763db4e6192d40bd1adcc2574797275bc6e849
covalwill <klimtgit@gmail.com>
2015-07-16 00:24:45 -0700

    added confirm password fucntionality

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
-$("form span").hide();
+$("form span").hide(); //Hides the span tag
 
 
+
+// function that will validate and check for a character length of 8. If there is a length of more than 8 it will hide the span tag. 
 function passwordEvent() {
 
-if ($(this).val().length > 8) {
+	if ($(this).val().length > 8) {
 		
 		$(this).next().hide();
-
 	} else {
+
+		$(this).next().show();
+	}
+}
+
+//
+function confirmPasswordEvent() {
+
+	if ($("password").val() === $("confirm_password").val()) {
+		
+		$(this).next().hide();
+	} else {
+
 		$(this).next().show();
 	}
 
 }
+ 
 
-
+//Password
 $("#password").focus(passwordEvent).keyup(passwordEvent);
+
+
+//Confirm Password
+$("#confirm_password").focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
